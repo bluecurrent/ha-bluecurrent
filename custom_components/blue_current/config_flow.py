50baa04407ba58e60dfacf7b818eb3f8504ab5b6
NickKoepr <nickkuiper44@gmail.com>
2025-06-12 16:02:06 +0200

    Updated button, config_flow, entity, sensor, strings to match Home Assistant integration.
    Currently, this branch is up-to-date with the Home Assistant integration
    as published for version 2025.6.0. Changes to the switches, start
    session button, trigger and device conditions will be added when merged
    by Home Assistant.
    
    Also changed the code owners and Blue Current API version in
    manifest.json.

--- custom_components/blue_current/config_flow.py
+++ custom_components/blue_current/config_flow.py
@@ -1,52 +1,44 @@
 """Config flow for Blue Current integration."""
+
 from __future__ import annotations
 
 from collections.abc import Mapping
 from typing import Any
 
-import voluptuous as vol
 from bluecurrent_api import Client
 from bluecurrent_api.exceptions import (
     AlreadyConnected,
     InvalidApiToken,
-    NoCardsFound,
     RequestLimitReached,
     WebsocketError,
 )
-from homeassistant import config_entries
-from homeassistant.const import CONF_API_TOKEN, CONF_ID
-from homeassistant.data_entry_flow import FlowResult
+import voluptuous as vol
 
-from .const import CARD, DOMAIN, LOGGER
+from homeassistant.config_entries import SOURCE_REAUTH, ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_API_TOKEN
 
-DATA_SCHEMA = vol.Schema(
-    {vol.Required(CONF_API_TOKEN): str, vol.Optional("add_card"): bool}
-)
-DEFAULT_CARD = "BCU_APP"
+from .const import DOMAIN, LOGGER
+
+DATA_SCHEMA = vol.Schema({vol.Required(CONF_API_TOKEN): str})
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class BlueCurrentConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle the config flow for Blue Current."""
 
     VERSION = 1
 
-    input: dict[str, Any]
-    client: Client
-    entry: config_entries.ConfigEntry | None
-
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         """Handle the initial step."""
-        self.client = Client()
         errors = {}
         if user_input is not None:
+            client = Client()
             api_token = user_input[CONF_API_TOKEN]
-            self._async_abort_entries_match({CONF_API_TOKEN: api_token})
 
             try:
-                await self.client.validate_api_token(api_token)
-                email = await self.client.get_email()
+                customer_id = await client.validate_api_token(api_token)
+                email = await client.get_email()
             except WebsocketError:
                 errors["base"] = "cannot_connect"
             except RequestLimitReached:
@@ -55,79 +47,32 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "already_connected"
             except InvalidApiToken:
                 errors["base"] = "invalid_token"
-            except Exception:  # pylint: disable=broad-except
+            except Exception:  # noqa: BLE001
                 LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
 
-            if not errors:
-                self.entry = await self.async_set_unique_id(email)
-                self.input = {CONF_API_TOKEN: api_token}
+            else:
+                if self.source != SOURCE_REAUTH:
+                    await self.async_set_unique_id(customer_id)
+                    self._abort_if_unique_id_configured()
+                    return self.async_create_entry(title=email, data=user_input)
 
-                if self.entry:
-                    await self.update_entry()
-                    return self.async_abort(reason="reauth_successful")
-                self._abort_if_unique_id_configured()
+                reauth_entry = self._get_reauth_entry()
+                if reauth_entry.unique_id == customer_id:
+                    return self.async_update_reload_and_abort(
+                        reauth_entry, data=user_input
+                    )
 
-                if user_input.get("add_card"):
-                    return await self.async_step_card()
-
-                self.input[CARD] = DEFAULT_CARD
-                return self.async_create_entry(
-                    title=user_input[CONF_API_TOKEN][:5], data=self.input
+                return self.async_abort(
+                    reason="wrong_account",
+                    description_placeholders={"email": email},
                 )
-
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
 
-    async def async_step_card(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the card step."""
-        errors = {}
-        api_token = self.input[CONF_API_TOKEN]
-        try:
-            cards = await self.client.get_charge_cards()
-        except WebsocketError:
-            errors["base"] = "cannot_connect"
-        except NoCardsFound:
-            errors["base"] = "no_cards_found"
-        except RequestLimitReached:
-            errors["base"] = "limit_reached"
-        except AlreadyConnected:
-            errors["base"] = "already_connected"
-        except Exception:  # pylint: disable=broad-except
-            LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-
-        if not errors:
-            card_ids = [card[CONF_ID] for card in cards]
-            card_schema = vol.Schema({vol.Required(CARD): vol.In(card_ids)})
-
-            def check_card(card: dict) -> bool:
-                # assert user_input is not None
-                return bool(card[CONF_ID] == user_input[CARD])
-
-            if user_input is not None:
-                selected_card = list(filter(check_card, cards))[0]
-
-                self.input[CARD] = selected_card["uid"]
-                return self.async_create_entry(title=api_token[:5], data=self.input)
-
-            return self.async_show_form(step_id=CARD, data_schema=card_schema)
-
-        return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
-        )
-
-    async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
+    async def async_step_reauth(
+        self, entry_data: Mapping[str, Any]
+    ) -> ConfigFlowResult:
         """Handle a reauthorization flow request."""
         return await self.async_step_user()
-
-    async def update_entry(self) -> None:
-        """Update the config entry."""
-        assert self.entry
-        self.hass.config_entries.async_update_entry(
-            self.entry, data=self.input, title=self.input[CONF_API_TOKEN][:5]
-        )
-        await self.hass.config_entries.async_reload(self.entry.entry_id)
