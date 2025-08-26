6c09dce8893fcc3a13d25f20977ef58499d7dc37
NickKoepr <nickkuiper44@gmail.com>
2025-08-26 14:25:54 +0200

    Updated init, const, strings and switches to match the current branch of home assistant.

--- custom_components/blue_current/const.py
+++ custom_components/blue_current/const.py
@@ -6,7 +6,16 @@ DOMAIN = "blue_current"
 
 LOGGER = logging.getLogger(__package__)
 
-ACTIVITY = "activity"
 EVSE_ID = "evse_id"
-CARD = "card"
 MODEL_TYPE = "model_type"
+PLUG_AND_CHARGE = "plug_and_charge"
+VALUE = "value"
+PERMISSION = "permission"
+CHARGEPOINT_STATUS = "CH_STATUS"
+CHARGEPOINT_SETTINGS = "CH_SETTINGS"
+BLOCK = "block"
+UNAVAILABLE = "unavailable"
+AVAILABLE = "available"
+LINKED_CHARGE_CARDS = "linked_charge_cards_only"
+PUBLIC_CHARGING = "public_charging"
+ACTIVITY = "activity"
